47b65399185c5586aaceccf20c68950f4fca8889
Seb-TheDropBears <seb.zanardo@thedropbears.org.au>
2024-04-14 17:28:06 +1000

    Note as to why this branch is deprecated

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import asyncio  # For running the game in browser
-
+import chess
+from stockfish import Stockfish
 from config.core import Core
 
+
+"""
+Most external libraries don't work with web compilation natively when using pygbag.
+Having a web build was priority for us since it this project is for a game jam.
+Therefore, we decided it was best to program our own chess engine as it gave us
+more freedom with board shapes and unique piece types and allowed us to compile for web.
+"""
 
 def main():
     app = Core()
