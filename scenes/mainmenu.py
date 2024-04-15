760af50ce5c2309daef95b92bd170d85260d9a9b
Seb-TheDropBears <seb.zanardo@thedropbears.org.au>
2024-04-16 02:32:27 +1000

    Added falling pieces to win screen

--- scenes/mainmenu.py
+++ scenes/mainmenu.py
@@ -8,40 +8,9 @@ from config.constants import WHITE, BACKGROUND
 from config.settings import WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_CENTRE
 from config.assets import CHESS_PIECES, GAME_FONT_BIG, GAME_FONT, GAME_FONT_SMALL
 from components.button import blit_centered_text
-
+from components.fallingpieces import FallingSprite
 # Import the whole module of all scenes you want to switch to
 import scenes.game
-
-
-class FallingSprite(pygame.sprite.Sprite):
-    def __init__(self, image, pos_x, pos_y, speed, rotate_speed) -> None:
-        self.original_image = image  # Store the original image for reference
-        self.image = image.copy()  # Create a working copy for rotation
-        self.rect = self.image.get_rect(center=(pos_x, pos_y))
-        self.speed_y = speed
-        self.angle = 0
-        self.rotate_speed = rotate_speed
-
-    def update(self, dt: float) -> None:
-        self.rect.y += self.speed_y * dt
-
-        # Calculate half width and half height
-        half_width = self.image.get_rect().width // 2
-        half_height = self.image.get_rect().height // 2
-
-        # Calculate maximum corner distance from center (diagonal)
-        max_distance = (half_width**2 + half_height**2) ** 0.5
-
-        # Calculate rotation speed based on desired speed and max distance
-        self.angle += (self.rotate_speed) / max_distance
-
-        # Rotate the working copy of the image using smoothscale
-        # This is where the performance hit happens.
-        self.image = pygame.transform.rotate(self.original_image, self.angle)
-
-        # Check for screen bottom and reset (optional)
-        if self.rect.top > WINDOW_HEIGHT:
-            self.rect.bottom = 0
 
 
 class MainMenu(Scene):
--- scenes/win.py
+++ scenes/win.py
@@ -1,13 +1,14 @@
+import random
 import pygame
 
 from utilities.typehints import ActionBuffer, MouseBuffer
 from config.input import InputState, MouseButton
 from baseclasses.scenemanager import Scene, SceneManager
-from config.settings import WINDOW_CENTRE
+from config.settings import WINDOW_CENTRE, WINDOW_HEIGHT, WINDOW_WIDTH
 from config.constants import WHITE, BACKGROUND
-from config.assets import GAME_FONT_BIG, GAME_FONT
+from config.assets import GAME_FONT_BIG, GAME_FONT, CHESS_PIECES
 from components.button import blit_centered_text
-
+from components.fallingpieces import FallingSprite
 # Import the whole module of all scenes you want to switch to
 import scenes.mainmenu
 
@@ -15,6 +16,30 @@ import scenes.mainmenu
 class Win(Scene):
     def __init__(self, scene_manager: SceneManager) -> None:
         super().__init__(scene_manager)
+
+        self.square_size = 100
+        self.piece_size = (self.square_size, self.square_size / 2 * 3)
+        self.scaled_pieces = [
+            pygame.transform.scale(sprite, self.piece_size) for sprite in CHESS_PIECES
+        ]
+        self.piece_offset = (0, -self.piece_size[1] // 2)
+        falling_pieces = self.scaled_pieces
+        self.pieces = []
+        for i, image in enumerate(falling_pieces):
+            x = random.randint(0, WINDOW_WIDTH)
+            y = random.randint(0, WINDOW_HEIGHT)
+            speed = random.randint(1, 4) * 50
+            # trust
+            rotate_speed = random.randint(25, 50) * random.choice((1, -1))
+            self.pieces.append(
+                FallingSprite(
+                    image=image,
+                    pos_x=x,
+                    pos_y=y,
+                    speed=speed,
+                    rotate_speed=rotate_speed,
+                )
+            )
 
         self.win_text = GAME_FONT_BIG.render("THE END!", False, WHITE)
         self.thanks_text = GAME_FONT.render(
@@ -31,10 +56,16 @@ class Win(Scene):
             self.scene_manager.switch_scene(scenes.mainmenu.MainMenu)
 
     def update(self, dt: float) -> None:
-        pass
+        for piece in self.pieces:
+            piece.update(dt)
+            if piece.rect.top > WINDOW_HEIGHT:
+                piece.rect.bottom = 0
 
     def render(self, surface: pygame.Surface) -> None:
         surface.fill(BACKGROUND)
+
+        for piece in self.pieces:
+            surface.blit(piece.image, piece.rect.topleft)
 
         blit_centered_text(surface, self.win_text, *WINDOW_CENTRE)
         blit_centered_text(
