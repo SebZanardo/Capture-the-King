01562e31ef7bd8a8eaa919bc89ad10a00053373d
Benjaminw104 <167047969+Benjaminw104@users.noreply.github.com>
2024-04-16 02:34:57 +1000

    Update levels.py

--- components/levels.py
+++ components/levels.py
@@ -7,93 +7,93 @@ levels = [
         "name": "Tut",
         "board": (30, 5, 5),
         "opponents": {
-            Colour.RED: [Piece.KING] + [Piece.PAWN] * 3,
+            Colour.RED: [Piece.PAWN] * 3,
         },
     },
     {
         "name": "Introducing Knights",
-        "board": (34, 7, 7),
+        "board": (44, 7, 7),
         "opponents": {
-            Colour.YELLOW: [Piece.KING] + [Piece.KNIGHT] * 2 + [Piece.PAWN] * 1,
-            Colour.RED: [Piece.KING] + [Piece.PAWN] * 3,
+            Colour.YELLOW: [Piece.KNIGHT] * 2 + [Piece.PAWN] * 1,
+            Colour.RED: [Piece.PAWN] * 3,
         },
     },
     {
-        "name": "Bishops on Fractal Board",
-        "board": (32, 9, 8),
+        "name": "Bishop Time",
+        "board": (112, 8, 8),
         "opponents": {
-            Colour.RED: [Piece.KING] + [Piece.PAWN] * 3,
-            Colour.BLUE: [Piece.KING] + [Piece.BISHOP] * 2 + [Piece.PAWN] * 2,
+            Colour.BLUE: [Piece.BISHOP] * 3 + [Piece.PAWN] * 6,
         },
     },
     {
         "name": "Close Quarters Rooks",
         "board": (50, 10, 4),
         "opponents": {
-            Colour.GREEN: [Piece.KING] + [Piece.ROOK] * 2,
-            Colour.RED: [Piece.KING] + [Piece.PAWN] * 3,
+            Colour.GREEN: [Piece.ROOK] * 2,
+            Colour.RED: [Piece.PAWN] * 3,
         },
     },
     {
         "name": "Queen Intro",
-        "board": (150, 15, 15),
+        "board": (150, 13, 13),
         "opponents": {
-            Colour.PURPLE: [Piece.KING] + [Piece.QUEEN] * 1 + [Piece.ROOK] * 2 + [Piece.KNIGHT] * 3 + [Piece.PAWN] * 5,
-            Colour.YELLOW: [Piece.KING] + [Piece.KNIGHT] * 2 + [Piece.PAWN] * 3,
+            Colour.PURPLE: [Piece.QUEEN] * 1 + [Piece.ROOK] * 2 + [Piece.KNIGHT] * 3 + [Piece.PAWN] * 5,
+            Colour.YELLOW: [Piece.KNIGHT] * 2 + [Piece.PAWN] * 3,
         },
     },
     {
         "name": "Bishops, Knights and Rooks",
-        "board": (175, 15, 15),
+        "board": (200, 15, 15),
         "opponents": {
-            Colour.BLUE: [Piece.KING] + [Piece.BISHOP] * 4,
-            Colour.YELLOW: [Piece.KING] + [Piece.KNIGHT] * 3 + [Piece.PAWN] * 4,
-            Colour.GREEN: [Piece.KING] + [Piece.PAWN] * 5 + [Piece.KNIGHT] * 2 + [Piece.ROOK] * 2
+            Colour.BLUE: [Piece.BISHOP] * 4,
+            Colour.YELLOW: [Piece.KNIGHT] * 3 + [Piece.PAWN] * 4,
+            Colour.GREEN: [Piece.PAWN] * 5 + [Piece.KNIGHT] * 2 + [Piece.ROOK] * 2
         },
     },
     {
-        "name": "Large Fractal",
-        "board": (75, 16, 16),
+        "name": "Black",
+        "board": (320, 16, 16),
         "opponents": {
-            Colour.BLUE: [Piece.KING] + [Piece.BISHOP] * 5,
-            Colour.PURPLE: [Piece.KING] + [Piece.QUEEN] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 6,
-            Colour.GREEN: [Piece.KING] + [Piece.PAWN] * 5 + [Piece.BISHOP] * 2 + [Piece.ROOK] * 3
+            Colour.BLACK: [Piece.BISHOP] * 6 + [Piece.KNIGHT] * 6 + [Piece.ROOK] * 6 + [Piece.PAWN] * 15 +
+                          [Piece.QUEEN] * 6,
+        },
+    },
+    {
+        "name": "Party",
+        "board": (144, 12, 12),
+        "opponents": {
+            Colour.BLUE: [Piece.BISHOP] * 5,
+            Colour.PURPLE: [Piece.QUEEN] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 6,
+            Colour.GREEN: [Piece.PAWN] * 5 + [Piece.BISHOP] * 2 + [Piece.ROOK] * 3
         },
     },
     {
         "name": "Classic Chess",
         "board": (210, 16, 16),
         "opponents": {
-            Colour.BLUE: [Piece.KING] + [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [Piece.ROOK] * 2 +
+            Colour.BLUE: [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [Piece.ROOK] * 2 +
                          [Piece.QUEEN],
-            Colour.RED: [Piece.KING] + [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [Piece.ROOK] * 2 +
+            Colour.RED: [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [Piece.ROOK] * 2 +
                         [Piece.QUEEN],
         },
     },
     {
         "name": "Classic Chess Mayhem",
-        "board": (1024, 32, 32),
+        "board": (400, 20, 20),
         "opponents": {
-            Colour.BLUE: [Piece.KING] + [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [Piece.ROOK] * 2 +
+            Colour.BLUE: [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [Piece.ROOK] * 2 +
                          [Piece.QUEEN],
-            Colour.RED: [Piece.KING] + [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [Piece.ROOK] * 2 +
+            Colour.RED: [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [Piece.ROOK] * 2 +
                         [Piece.QUEEN],
-            Colour.YELLOW: [Piece.KING] + [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [
+            Colour.YELLOW: [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [
                 Piece.ROOK] * 2 +
                            [Piece.QUEEN],
-            Colour.GREEN: [Piece.KING] + [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [Piece.ROOK] * 2 +
+            Colour.GREEN: [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [Piece.ROOK] * 2 +
                           [Piece.QUEEN],
-            Colour.PURPLE: [Piece.KING] + [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [
+            Colour.PURPLE: [Piece.KNIGHT] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 8 + [
                 Piece.ROOK] * 2 +
                            [Piece.QUEEN],
         },
     },
-    {
-        "name": "Black",
-        "board": (640, 32, 32),
-        "opponents": {
-            Colour.BLACK: [Piece.KING] + [Piece.BISHOP] * 6 + [Piece.KNIGHT] * 6 + [Piece.ROOK] * 6 + [Piece.PAWN] * 6 +
-                          [Piece.QUEEN] * 6,
-        },
-    },
+
 ]
