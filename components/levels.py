d0960002d9439e8a2a7bd81d87d6dabc2e65fec9
Seb-TheDropBears <seb.zanardo@thedropbears.org.au>
2024-04-16 08:32:01 +1000

    Balancing of levels and new names

--- components/levels.py
+++ components/levels.py
@@ -3,130 +3,163 @@ from components.chess import Colour, Piece
 
 levels = [
     {
-        "name": "Tut",
-        "board": (30, 5, 5),
+        "name": "HUMBLE BEGINNINGS",
+        "board": (25, 5, 5),
         "opponents": {
             Colour.RED: [Piece.PAWN] * 3,
+        },
+        "mana_awarded": 15,
+    },
+    {
+        "name": "JUMPING KNIGHTS",
+        "board": (36, 6, 6),
+        "opponents": {
+            Colour.YELLOW: [Piece.KNIGHT] * 2 + [Piece.PAWN] * 2,
         },
         "mana_awarded": 20,
     },
     {
-        "name": "Introducing Knights",
+        "name": "BEGINNER DUO",
         "board": (44, 7, 7),
         "opponents": {
+            Colour.RED: [Piece.PAWN] * 3,
             Colour.YELLOW: [Piece.KNIGHT] * 2 + [Piece.PAWN] * 1,
-            Colour.RED: [Piece.PAWN] * 3,
         },
         "mana_awarded": 20,
     },
     {
-        "name": "Bishop Time",
-        "board": (112, 8, 8),
+        "name": "DIAGONAL DANGER",
+        "board": (36, 6, 6),
         "opponents": {
-            Colour.BLUE: [Piece.BISHOP] * 3 + [Piece.PAWN] * 6,
+            Colour.GREEN: [Piece.BISHOP] * 3 + [Piece.PAWN] * 5,
         },
         "mana_awarded": 20,
     },
     {
-        "name": "Close Quarters Rooks",
-        "board": (50, 10, 4),
+        "name": "DYNAMIC DUO",
+        "board": (70, 9, 9),
         "opponents": {
-            Colour.GREEN: [Piece.ROOK] * 2,
-            Colour.RED: [Piece.PAWN] * 3,
-        },
-        "mana_awarded": 20,
-    },
-    {
-        "name": "Queen Intro",
-        "board": (150, 13, 13),
-        "opponents": {
-            Colour.PURPLE: [Piece.QUEEN] * 1
-            + [Piece.ROOK] * 2
-            + [Piece.KNIGHT] * 3
-            + [Piece.PAWN] * 5,
-            Colour.YELLOW: [Piece.KNIGHT] * 2 + [Piece.PAWN] * 3,
-        },
-        "mana_awarded": 20,
-    },
-    {
-        "name": "Bishops, Knights and Rooks",
-        "board": (200, 15, 15),
-        "opponents": {
-            Colour.BLUE: [Piece.BISHOP] * 4,
             Colour.YELLOW: [Piece.KNIGHT] * 3 + [Piece.PAWN] * 4,
-            Colour.GREEN: [Piece.PAWN] * 5 + [Piece.KNIGHT] * 2 + [Piece.ROOK] * 2,
+            Colour.GREEN: [Piece.BISHOP] * 3 + [Piece.PAWN] * 2,
         },
         "mana_awarded": 20,
     },
     {
-        "name": "Black",
-        "board": (320, 16, 16),
+        "name": "RELENTLESS ROOKS",
+        "board": (36, 6, 6),
         "opponents": {
-            Colour.BLACK: [Piece.BISHOP] * 6
-            + [Piece.KNIGHT] * 6
-            + [Piece.ROOK] * 6
-            + [Piece.PAWN] * 15
-            + [Piece.QUEEN] * 6,
+            Colour.BLUE: [Piece.ROOK] * 3,
         },
         "mana_awarded": 20,
     },
     {
-        "name": "Party",
-        "board": (144, 12, 12),
+        "name": "SLIDING PIECE SAGA",
+        "board": (80, 10, 10),
         "opponents": {
-            Colour.BLUE: [Piece.BISHOP] * 5,
-            Colour.PURPLE: [Piece.QUEEN] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 6,
-            Colour.GREEN: [Piece.PAWN] * 5 + [Piece.BISHOP] * 2 + [Piece.ROOK] * 3,
+            Colour.GREEN: [Piece.BISHOP] * 2 + [Piece.PAWN]*3,
+            Colour.BLUE: [Piece.ROOK] * 2 + [Piece.PAWN]*3,
         },
         "mana_awarded": 20,
     },
     {
-        "name": "Classic Chess",
-        "board": (210, 16, 16),
+        "name": "MINOR PIECE TRIO",
+        "board": (84, 12, 8),
         "opponents": {
-            Colour.BLUE: [Piece.KNIGHT] * 2
-            + [Piece.BISHOP] * 2
-            + [Piece.PAWN] * 8
-            + [Piece.ROOK] * 2
-            + [Piece.QUEEN],
-            Colour.RED: [Piece.KNIGHT] * 2
-            + [Piece.BISHOP] * 2
-            + [Piece.PAWN] * 8
-            + [Piece.ROOK] * 2
-            + [Piece.QUEEN],
+            Colour.YELLOW: [Piece.KNIGHT] * 2 + [Piece.PAWN]*3,
+            Colour.GREEN: [Piece.BISHOP] * 2 + [Piece.PAWN]*3,
+            Colour.BLUE: [Piece.ROOK] * 2 + [Piece.PAWN]*4,
         },
         "mana_awarded": 20,
     },
     {
-        "name": "Classic Chess Mayhem",
-        "board": (400, 20, 20),
+        "name": "THE ALMIGHTY QUEEN",
+        "board": (64, 8, 8),
         "opponents": {
-            Colour.BLUE: [Piece.KNIGHT] * 2
-            + [Piece.BISHOP] * 2
-            + [Piece.PAWN] * 8
-            + [Piece.ROOK] * 2
-            + [Piece.QUEEN],
-            Colour.RED: [Piece.KNIGHT] * 2
-            + [Piece.BISHOP] * 2
-            + [Piece.PAWN] * 8
-            + [Piece.ROOK] * 2
-            + [Piece.QUEEN],
-            Colour.YELLOW: [Piece.KNIGHT] * 2
-            + [Piece.BISHOP] * 2
-            + [Piece.PAWN] * 8
-            + [Piece.ROOK] * 2
-            + [Piece.QUEEN],
-            Colour.GREEN: [Piece.KNIGHT] * 2
-            + [Piece.BISHOP] * 2
-            + [Piece.PAWN] * 8
-            + [Piece.ROOK] * 2
-            + [Piece.QUEEN],
-            Colour.PURPLE: [Piece.KNIGHT] * 2
-            + [Piece.BISHOP] * 2
-            + [Piece.PAWN] * 8
-            + [Piece.ROOK] * 2
-            + [Piece.QUEEN],
+            Colour.PURPLE: [Piece.QUEEN] * 3 + [Piece.PAWN] * 3,
         },
         "mana_awarded": 20,
     },
+    {
+        "name": "THE FINAL CHALLENGE",
+        "board": (150, 20, 10),
+        "opponents": {
+            Colour.YELLOW: [Piece.KNIGHT] * 3 + [Piece.PAWN]*3,
+            Colour.GREEN: [Piece.BISHOP] * 3 + [Piece.PAWN]*3,
+            Colour.BLUE: [Piece.ROOK] * 2 + [Piece.PAWN] * 3,
+            Colour.PURPLE: [Piece.QUEEN] * 2 + [Piece.PAWN] * 3,
+            Colour.BLACK: [Piece.KING] * 4 + [Piece.QUEEN] + [Piece.PAWN] * 8,
+        },
+        "mana_awarded": 20,
+    },
+    # {
+    #     "name": "Black",
+    #     "board": (320, 16, 16),
+    #     "opponents": {
+    #         Colour.BLACK: [Piece.BISHOP] * 6
+    #         + [Piece.KNIGHT] * 6
+    #         + [Piece.ROOK] * 6
+    #         + [Piece.PAWN] * 15
+    #         + [Piece.QUEEN] * 6,
+    #     },
+    #     "mana_awarded": 20,
+    # },
+    # {
+    #     "name": "Party",
+    #     "board": (144, 12, 12),
+    #     "opponents": {
+    #         Colour.BLUE: [Piece.BISHOP] * 5,
+    #         Colour.PURPLE: [Piece.QUEEN] * 2 + [Piece.BISHOP] * 2 + [Piece.PAWN] * 6,
+    #         Colour.GREEN: [Piece.PAWN] * 5 + [Piece.BISHOP] * 2 + [Piece.ROOK] * 3,
+    #     },
+    #     "mana_awarded": 20,
+    # },
+    # {
+    #     "name": "Classic Chess",
+    #     "board": (210, 16, 16),
+    #     "opponents": {
+    #         Colour.BLUE: [Piece.KNIGHT] * 2
+    #         + [Piece.BISHOP] * 2
+    #         + [Piece.PAWN] * 8
+    #         + [Piece.ROOK] * 2
+    #         + [Piece.QUEEN],
+    #         Colour.RED: [Piece.KNIGHT] * 2
+    #         + [Piece.BISHOP] * 2
+    #         + [Piece.PAWN] * 8
+    #         + [Piece.ROOK] * 2
+    #         + [Piece.QUEEN],
+    #     },
+    #     "mana_awarded": 20,
+    # },
+    # {
+    #     "name": "Classic Chess Mayhem",
+    #     "board": (400, 20, 20),
+    #     "opponents": {
+    #         Colour.BLUE: [Piece.KNIGHT] * 2
+    #         + [Piece.BISHOP] * 2
+    #         + [Piece.PAWN] * 8
+    #         + [Piece.ROOK] * 2
+    #         + [Piece.QUEEN],
+    #         Colour.RED: [Piece.KNIGHT] * 2
+    #         + [Piece.BISHOP] * 2
+    #         + [Piece.PAWN] * 8
+    #         + [Piece.ROOK] * 2
+    #         + [Piece.QUEEN],
+    #         Colour.YELLOW: [Piece.KNIGHT] * 2
+    #         + [Piece.BISHOP] * 2
+    #         + [Piece.PAWN] * 8
+    #         + [Piece.ROOK] * 2
+    #         + [Piece.QUEEN],
+    #         Colour.GREEN: [Piece.KNIGHT] * 2
+    #         + [Piece.BISHOP] * 2
+    #         + [Piece.PAWN] * 8
+    #         + [Piece.ROOK] * 2
+    #         + [Piece.QUEEN],
+    #         Colour.PURPLE: [Piece.KNIGHT] * 2
+    #         + [Piece.BISHOP] * 2
+    #         + [Piece.PAWN] * 8
+    #         + [Piece.ROOK] * 2
+    #         + [Piece.QUEEN],
+    #     },
+    #     "mana_awarded": 20,
+    # },
 ]
--- scenes/globals.py
+++ scenes/globals.py
@@ -1,4 +1,4 @@
 # Put your persistent scene data in this script!
 level = 0
-starting_mana = 50
+starting_mana = 10
 mana = starting_mana
